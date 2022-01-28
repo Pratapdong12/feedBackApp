04d4cf5341ef5de7aca8204356d79c5b7a39bc6a
Pratapdong12 <angdong9@gmail.com>
2022-01-28 09:53:17 +0545

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import Header from './components/Header';
-import { FeedbackData } from './data/FeedbackData';
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
-import Button from './components/shared/Button';
-import { v4 as uuidv4} from 'uuid';
+import React from "react";
+import Header from "./components/Header";
+import { FeedbackData } from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [feedback, setFeedback] = React.useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback( [...feedback, newFeedback]);
+    setFeedback([...feedback, newFeedback]);
   };
 
   return (
     <>
       <Header />
-      <div className='container'>
-        <FeedbackForm handleAdd={addFeedback}/>
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
