e597e6f35ce65ef85426144b08fecbe69c3a2abe
Pratapdong12 <angdong9@gmail.com>
2022-01-31 09:37:09 +0545

    react router dom added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FeedbackData } from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -22,14 +23,21 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Route
+          path="/"
+          element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }
+        ></Route>
       </div>
-    </>
+    </Router>
   );
 }
 
